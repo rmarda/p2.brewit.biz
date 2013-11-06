f63d1445bece293460d3f92d1571dc01c966eeda
Ruchika Marda <rmarda4@gmail.com>
2013-11-06 16:08:29 -0500

    deleted files not needed

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -5,12 +5,11 @@
                 <li class="comment">
                     <blockquote>
                         <p>Welcome <?= $user->first_name ?> <?= $user->last_name ?>! Here are a few helpful tips to get your started:</p>
-                        <p>Navigate around this website using the menu items above.</p>
-                        <p>Profile: Brings you right here!</p>
-                        <p>All users: View all users signed up for Nano blog and follow them to read their posts.</p>
-                        <p>Posts: View posts from all your followed users.</p>
-                        <p>Add Posts: Add your posts from here</p>
-                        <p>Manage Posts: You can edit or delete an existing post from here.</p>
+                        <p><span style="color: darkgreen;">Profile:</span> Brings you right here!</p>
+                        <p><span style="color: darkgreen;">All users:</span> View all users signed up for Nano blog and follow them to read their posts.</p>
+                        <p><span style="color: darkgreen;">Posts: </span>View posts from all your followed users.</p>
+                        <p><span style="color: darkgreen;">Add Posts: </span>Add your posts from here</p>
+                        <p><span style="color: darkgreen;">Manage Posts: </span>You can edit or delete an existing post from here.</p>
                     </blockquote>
                 </li>
             </ol>
